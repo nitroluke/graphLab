224ba2e90d7dd146ee003cf909eb6931b3d6d3ce
nitroluke <nitro.luke@gmail.com>
2014-03-04 21:41:16 -0700

    Completed Breadth First Search and Depth First Search

--- src/graph/Driver.java
+++ src/graph/Driver.java
@@ -1,6 +1,5 @@
 package graph;
 
-
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.Scanner;
@@ -11,44 +10,45 @@ import java.util.Scanner;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author nitroluke
  */
 public class Driver {
-    
-        /**
+
+    /**
      * @param args the command line arguments
      * @throws java.io.FileNotFoundException
      */
     public static void main(String[] args) throws FileNotFoundException {
         int arrLength = 5;
-        int x = 0; 
+        int x = 0;
         int y = 0;
-        
+        int start = 0;  // which node do you want to start to search from
+
         char value;
         // TODO code application logic here
         Scanner in = new Scanner(new FileReader("C://Users/nitroluke/Desktop/AdjacencyMatrix.txt"));
 //        in.useDelimiter("");
         int adjacencyMatrix[][] = new int[arrLength][arrLength];
-        boolean bRepresentation[][] = new boolean[arrLength][arrLength];
-        while(in.hasNext()){
+//        boolean bRepresentation[][] = new boolean[arrLength][arrLength];
+
+        while (in.hasNext()) {
             String string = in.next();
-            for(int i = 0; i < string.length(); i++){
-                if (y > 4){ // go to the next line reset y, and increment x.
-                x++;
-                y = 0; 
+            for (int i = 0; i < string.length(); i++) {
+                if (y > 4) { // go to the next line reset y, and increment x.
+                    x++;
+                    y = 0;
                 }
                 value = string.charAt(i);
-                switch(value){  // doing this becuase it was still using the ASCII values every after the type cast.
-                    case 48: 
+                switch (value) {  // doing this becuase it was still using the ASCII values every after the type cast.
+                    case 48:
                         adjacencyMatrix[x][y] = 0;
-                        bRepresentation[x][y] = false;
+//                        bRepresentation[x][y] = false;
                         break;
-                    case 49: 
+                    case 49:
                         adjacencyMatrix[x][y] = 1;
-                        bRepresentation[x][y] = true;
+//                        bRepresentation[x][y] = true;
                         break;
                     default:
                         System.out.println("This should never happen");
@@ -56,14 +56,27 @@ public class Driver {
                 }
                 y++;
             }
-          Graph g = new Graph(adjacencyMatrix);  
-          g.BFS(0);
+
 //            System.out.println("y = " + y + " x = " + x);
         }
-        for(int i = 0; i < adjacencyMatrix.length; i ++){
-            for (int j = 0; j < adjacencyMatrix.length; j++){
+        Graph g = new Graph(adjacencyMatrix);
+
+        System.out.println("\nDoing Breadth First Search starting at " + start);
+        System.out.println("---------------------------------------\n");
+        g.BFS(start);
+        System.out.println("\nFinished Breadth First Search");
+        System.out.println("_______________________________________\n");
+
+        System.out.println("\nDoing Depth First Search starting at 0");
+        System.out.println("---------------------------------------\n");
+        g.DFS(start);
+        System.out.println("\nFinished Breadth First Search");
+        System.out.println("_______________________________________\n");
+        
+        for (int i = 0; i < adjacencyMatrix.length; i++) {
+            for (int j = 0; j < adjacencyMatrix.length; j++) {
                 System.out.print(adjacencyMatrix[i][j]);
-                
+
             }
             System.out.println("");
         }
--- src/graph/Graph.java
+++ src/graph/Graph.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package graph;
 
 import java.util.LinkedList;
@@ -14,30 +13,50 @@ import java.util.Queue;
  * @author nitroluke
  */
 public class Graph {
-private int adjMatrix[][];
-public Graph(int adjMatrix[][]){
-    this.adjMatrix = adjMatrix;
-    
-}
-public void BFS(int start){  // do this using a queue
- Queue<Integer> queue = new LinkedList<>();
- boolean visited[] = new boolean[adjMatrix.length * adjMatrix.length];
 
- queue.offer(start);
- visited[start] = true;
- while(!queue.isEmpty()){
-    int current = queue.poll();
-     System.out.println("The current node we are at is " + current);    
-    for(int y = 0; y < adjMatrix.length; y++){
-        if(adjMatrix[current][y] == 1 && !visited[y]){
-            queue.offer(y);
-            visited[y] = true;
-            System.out.println(" BFS - Adding node " + y + " to the queue" );
-     }
-        
+    private int adjMatrix[][];
+
+    public Graph(int adjMatrix[][]) {
+        this.adjMatrix = adjMatrix;
     }
-     System.out.println("Done processing node " + current);
- }
 
-}
+    public void BFS(int start) {  // do this using a queue
+        Queue<Integer> queue = new LinkedList<>();
+        boolean visited[] = new boolean[adjMatrix.length * adjMatrix.length];
+
+        queue.offer(start);
+        visited[start] = true;
+        while (!queue.isEmpty()) {
+            int current = queue.poll();
+            System.out.println("The current node we are at is " + current);
+            for (int y = 0; y < adjMatrix.length; y++) {
+                if (adjMatrix[current][y] == 1 && !visited[y]) {
+                    queue.offer(y);
+                    visited[y] = true;
+                    System.out.println("Adding node " + y + " to the queue");
+                }
+
+            }
+            System.out.println("Done processing node " + current);
+        }
+    }
+
+    private void DFS(int current, boolean visited[]) {
+        System.out.println("The current node we are at is " + current);
+        visited[current] = true;
+
+        for (int y = 0; y < adjMatrix.length; y++) {
+            if (adjMatrix[current][y] == 1 && !visited[y]) {
+                System.out.println("Going to Node " + y);
+                DFS(y, visited);
+            }
+        }
+        System.out.println("Done processing node " + current + " Bitch!");
+    }
+    
+    public void DFS(int start){
+    boolean visitedArr[] = new boolean[adjMatrix.length];
+
+        DFS(start, visitedArr);
+    }
 }
